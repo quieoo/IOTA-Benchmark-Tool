ee62445a6baa0a92118bd4ba13b90b529a2c50db
quieoo <715094648@qq.com>
2023-05-22 16:34:41 +0800

    get iri..

--- IOTA/get_iri.sh
+++ IOTA/get_iri.sh
@@ -2,3 +2,4 @@ scp quieoo@192.168.1.124://home/quieoo/Desktop/ABACUS/start.sh ./
 scp quieoo@192.168.1.124://home/quieoo/Desktop/ABACUS/snapshot.txt ./
 mkdir target
 scp quieoo@192.168.1.124://home/quieoo/Desktop/ABACUS/target/iri-1.8.1.jar ./target/iri-1.8.1.jar
+chmod +x start.sh
